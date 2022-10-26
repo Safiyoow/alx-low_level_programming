60dc992ec48b7356d1af503f02fa9484101477f5
Safiyoow <izabelasharif@gmail.com>
2022-10-26 05:34:06 -0700

    t

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,36 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - Print whole the hash table.
- * @ht: Hash table.
+ * hash_table_delete - deletes a hash table
  *
- * Return: Void.
+ * @ht: pointer to the hash table
+ * Return: no return
  */
-void hash_table_print(const hash_table_t *ht)
+void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i, flag = 0;
-	hash_node_t *current;
+	unsigned long int i;
+	hash_node_t *tmp1;
+	hash_node_t *tmp2;
 
 	if (ht == NULL)
 		return;
-	putchar('{');
+
 	for (i = 0; i < ht->size; i++)
 	{
-		current = ht->array[i];
-		if (current != NULL)
+		tmp1 = ht->array[i];
+		while ((tmp2 = tmp1) != NULL)
 		{
-			while (current != NULL)
-			{
-				if (flag == 1)
-				{
-					printf(", ");
-				}
-				printf("'%s': '%s'", current->key, current->value);
-				flag = 1;
-				current = current->next;
-			}
+			tmp1 = tmp1->next;
+			free(tmp2->key);
+			free(tmp2->value);
+			free(tmp2);
 		}
 	}
-	putchar('}');
-	putchar('\n');
+	free(ht->array);
+	free(ht);
 }
